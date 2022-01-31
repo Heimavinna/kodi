d1e40f8eeeb8d0209bc5593a6f0e421f568ee2f5
xJod <jonasorridan@gmail.com>
2022-01-31 11:52:57 +0000

    jonas branch push test

--- hello.py
+++ hello.py
@@ -7,9 +7,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-
-
     return render_template("index.html")
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html', message='Epic message')
+
+
 
 @app.route('/sida2', methods=['GET', 'POST'])
 def sida2():
