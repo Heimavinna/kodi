fa0ecc4e44913a4ba0a797bc6804ca10f416729e
snorri <snorrimar4@hotmail.com>
2022-02-15 16:44:28 +0000

    css

--- app.py
+++ app.py
@@ -43,6 +43,10 @@ def delete(todo_id):
     return redirect(url_for("index"))
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 
 if __name__ == '__main__':
     db.create_all()
